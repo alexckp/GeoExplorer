67dd3e9f40008c254061c9160fa1170a6879d6e1
Santiago Domínguez <Santiago.Dominguez@carm.es>
2011-12-09 11:01:27 +0100

    Añadida opción &l=true para forzar a hacer login al entrar Añadido opción &url=<URL> para que redirija a la URL en cuestión después de hacer login

--- app/static/script/app/GeoExplorer/Composer.js
+++ app/static/script/app/GeoExplorer/Composer.js
@@ -238,6 +238,11 @@ GeoExplorer.Composer = Ext.extend(GeoExplorer, {
                     this.setCookieValue(this.cookieParamName, user);
                     this.showLogout(user);
                     win.close();
+                    if (urlParams["URL"]) {
+                      window.location = urlParams["URL"];
+                      location.reload();
+                    }
+
                 },
                 failure: function(form, action) {
                     this.authorizedRoles = [];
